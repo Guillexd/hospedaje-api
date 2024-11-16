ab1c2c387ad10d366cd966af793c6f05dd7b7322
GuillexD <enrique2003junior@gmail.com>
2024-11-16 15:41:23 -0500

    seeder fixed in housingPayment

--- database/factories/HousingPaymentFactory.php
+++ database/factories/HousingPaymentFactory.php
@@ -19,18 +19,18 @@ class HousingPaymentFactory extends Factory
      */
     public function definition(): array
     {
-        $housing_id = HousingRoom::inRandomOrder()->where('is_active', 1)->first()->id;
+        $housing = HousingRoom::inRandomOrder()->where('is_active', 1)->first();
         $rentalStartDate = fake()->dateTimeBetween('-4 months', 'now');
         $rentalEndDate = Carbon::instance($rentalStartDate)->addMonth()->format('Y-m-d H:i:s');
         $paymentDate = rand(1, 10) === 10 ? null : $rentalEndDate;
         $rentalStartDate = Carbon::instance($rentalStartDate)->format('Y-m-d H:i:s');
 
         if(is_null($paymentDate)) {
-            HousingRoom::find($housing_id)->update(['is_available' => 0]);
+            $housing->update(['is_available' => 0]);
         }
 
         return [
-            'housing_room_id' => $housing_id,
+            'housing_room_id' => $housing->id,
             'tenancy_id' => Tenancy::inRandomOrder()->first()->id,
             'payment' => fake()->randomFloat(2, 200, 500),
             'rental_start_date' => $rentalStartDate,
